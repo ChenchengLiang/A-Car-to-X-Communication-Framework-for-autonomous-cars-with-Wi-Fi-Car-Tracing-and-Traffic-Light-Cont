afc6eb514db68d48649606f3c81cfd7c26a1e4e8
lcckkkhaha <lcckkkhaha@163.com>
2017-05-28 23:31:19 +0200

    Update install.sh

--- Code/install.sh
+++ Code/install.sh
@@ -2,3 +2,15 @@
 sudo apt-get install python3-pip
 sudo pip install selenium
 sudo pip install uuid
+
+
+//To install MQTT for Python install below 
+pip install paho-mqtt 
+sudo wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key 
+sudo apt-key add mosquitto-repo.gpg.key 
+cd /etc/apt/sources.list.d/ 
+sudo wget http://repo.mosquitto.org/debian/mosquitto-jessie.list 
+sudo apt-get update 
+sudo apt-get install mosquito 
+sudo apt-get install mosquitto mosquitto-clients python-mosquito 
+sudo /etc/init.d/mosquitto stop 
